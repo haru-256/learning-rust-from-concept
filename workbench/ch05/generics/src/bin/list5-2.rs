6ff9402979f35ec4e2c976250d4e2b26dce97718
Yohei Okabayashi <yohei.kuro48@gmail.com>
2024-08-19 19:40:54 +0900

    chap05

---
+++ workbench/ch05/generics/src/bin/list5-2.rs
@@ -0,0 +1,21 @@
+struct GenEx<T> {
+    value: T,
+}
+
+impl<T> GenEx<T> {
+    fn get_value(self) -> T {
+        self.value
+    }
+}
+
+fn main() {
+    let x1 = GenEx { value: 42 };
+    let x2 = GenEx {
+        value: String::from("Hello"),
+    };
+    let x3 = GenEx { value: 3.14 };
+    let x3 = GenEx::<f32> { value: 3.14 };
+    println!("x1: {}", x1.get_value());
+    println!("x2: {}", x2.get_value());
+    println!("x3: {}", x3.get_value());
+}
---
+++ workbench/ch05/generics/src/main.rs
@@ -0,0 +1,12 @@
+fn return_input<T>(x: T) -> T {
+    x
+}
+
+fn main() {
+    let x1 = return_input(42);
+    let x2 = return_input(String::from("Hello Value"));
+    let x3 = return_input(3.14);
+    println!("x1: {}", x1);
+    println!("x2: {}", x2);
+    println!("x3: {}", x3);
+}
