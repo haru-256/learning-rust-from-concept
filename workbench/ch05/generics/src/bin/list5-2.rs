92de3e0b9b1ef2cc199173e185746ef131cea554
Yohei Okabayashi <yohei.kuro48@gmail.com>
2025-03-25 23:19:15 +0900

    ADD: update generics example to use constant for PI and add additional variable

--- workbench/ch05/generics/src/bin/list5-2.rs
+++ workbench/ch05/generics/src/bin/list5-2.rs
@@ -13,9 +13,12 @@ fn main() {
     let x2 = GenEx {
         value: String::from("Hello"),
     };
-    let x3 = GenEx { value: 3.14 };
-    let x3 = GenEx::<f32> { value: 3.14 };
+    let x3 = GenEx {
+        value: std::f32::consts::PI,
+    };
+    let x4 = GenEx::<f32> { value: 3.14 };
     println!("x1: {}", x1.get_value());
     println!("x2: {}", x2.get_value());
     println!("x3: {}", x3.get_value());
+    println!("x4: {}", x4.get_value());
 }
