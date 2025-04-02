bd4ef38f5ce846bd2d1e64f3178e361c49032ddc
Yohei Okabayashi <yohei.kuro48@gmail.com>
2025-04-02 20:31:09 +0900

    ADD: add byteorder dependency in Cargo.toml; update list6-3.rs to print lines read from file

--- workbench/ch06/file-io/src/bin/list6-3.rs
+++ workbench/ch06/file-io/src/bin/list6-3.rs
@@ -5,18 +5,18 @@ use std::io::{self, BufRead, BufReader};
 fn main() -> io::Result<()> {
     let f = File::open("hello.txt")?;
     let f = BufReader::new(f);
-
     let mut lines_vec = Vec::new();
-
     for line in f.lines() {
         match line {
             Ok(l) => lines_vec.push(l),
             Err(e) => return Err(e),
         }
     }
+    println!("{:?}", lines_vec);
 
     let f = File::open("hello.txt")?;
     let mut f = BufReader::new(f);
+    let mut lines_vec = Vec::new();
     loop {
         let mut ll = String::new();
         let read_size = f.read_line(&mut ll)?;
